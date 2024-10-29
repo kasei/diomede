9558d5e5b13cd7600eab2dfabb0df7752419e784
Gregory Todd Williams <greg@evilfunhouse.com>
2024-10-29 13:05:54 -0700

    Update swift-tools and macOS version.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Diomede",
-    platforms: [.macOS(.v10_15)],
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
